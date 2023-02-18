e6cc49cd7618c2caaa242ae2395368ac0637b665
Samrat Shahajahan <sumonskys@gmail.com>
2023-02-18 12:05:42 +0600

    add some function

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // Export Current Time Stamp
-export const TimeStamp = () => new Date().getTime();
+export const TimeStamp = ({ prefix = "", suffix = "" } = {}) => prefix + new Date().getTime().toString() + suffix;
 
 // Unique number with current timestamp
-export const Unique = () =>
-  new Date().getTime() + Math.floor(Math.random() * 100) + 1;
+export const Unique = ({ prefix = "", suffix = "" } = {}) =>
+  prefix + (new Date().getTime().toString() + Math.floor(Math.random() * 100) + 1).toString() + suffix;
 
 // Ellipsis for String
-export const Ellipsis = (input, size = 30) =>
-  input.length > size ? `${input.substring(0, size)}...` : input;
+export const Ellipsis = (input, length = 10, ellipsisCount = 3) => {
+  const text = input.toString();
+  if (isNaN(length) || isNaN(ellipsisCount)) {
+    console.error("length and ellipsisCount must be valid numbers");
+    return;
+  }
+
+  if (text.length <= length) {
+    return text;
+  }
+  const ellipsis = Array.from(Array(ellipsisCount))
+    .map(() => ".")
+    .join("");
+  return `${text.substr(0, length)}${ellipsis}`;
+};
 
 // Convert Second to Countdown time (Hour: Minute: Second)
 export const ConvertHMS = (value) => {
@@ -48,15 +61,15 @@ export const ConvertMS = (value) => {
 };
 
 // Close Window
-export const WindowClose = () => {
+export const WindowClose = (redirect = "") => {
   window.open("about:blank", "_self");
   window.close();
-  window.location.replace("about:blank");
-  return;
+  if (redirect) return window.location.replace(redirect);
+  return window.location.replace("about:blank");
 };
 
-// Camel Case
-export const CamelCase = (string) => {
+// First Letter Upper Case
+export const UCFirst = (string) => {
   if (string) return string.charAt(0).toUpperCase() + string.slice(1);
   return false;
 };
@@ -68,9 +81,110 @@ export const ValidateEmail = (email) => {
   return re.test(String(email).toLowerCase());
 };
 
-// Sleep 
-export const sleep = async (milliseconds) => {
+// Sleep
+export const Sleep = async (second) => {
   await new Promise((resolve) => {
-    return setTimeout(resolve, milliseconds);
+    return setTimeout(resolve, parseInt(second) * 1000);
   });
+};
+
+// Run function multiple time
+export const RunFunc = (func, times = 2) => {
+  if (isNaN(times)) {
+    console.error("times to run must be specified");
+    return;
+  }
+  if (typeof func !== "function") {
+    console.error(`func must be a valid function, ${typeof func} provided`);
+    return;
+  }
+  Array.from(Array(times)).forEach(() => {
+    func();
+  });
+};
+
+// Run function after specific time
+export const RunDelayFunc = (func, timeout = 2) => {
+  if (typeof func !== "function") {
+    console.error(`func must be a valid function, ${typeof func} provided`);
+    return;
+  }
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout * 1000);
+  };
+};
+
+// Random number generate with range
+export const RandomRange = (lower = 0, upper = 100) => {
+  if (isNaN(lower) || isNaN(upper)) {
+    console.error("lower and upper must be valid numbers");
+    return;
+  }
+  lower = Math.ceil(lower);
+  upper = Math.floor(upper);
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+};
+
+// Remove duplicate from array
+export const RDFA = (arr) => {
+  if (!Array.isArray(arr)) {
+    console.error(`array expected, ${typeof arr} provided`);
+    return;
+  }
+  return [...new Set(arr)];
+};
+
+// Function runtime
+export const FuncRunTime = (func, label = "default") => {
+  if (typeof func !== "function") {
+    console.error(`func must be a valid function, ${typeof func} provided`);
+    return;
+  }
+  console.time(label);
+  func();
+  console.timeEnd(label);
+};
+
+// Slugify
+export const Slugify = (text) => {
+  if (!(typeof text === "string" || text instanceof String)) {
+    console.error(`string expected, ${typeof str} provided`);
+    return str;
+  }
+  return text
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
+};
+
+// Camel case to snake case
+export const CCTSC = (text) => {
+  if (!(typeof text === "string" || text instanceof String)) {
+    console.error(`string expected, ${typeof text} provided`);
+    return text;
+  }
+  return text.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+};
+
+// Snake case to camel case
+export const SCTCC = (text) => {
+  if (!(typeof text === "string" || text instanceof String)) {
+    console.error(`string expected, ${typeof text} provided`);
+    return text;
+  }
+  return text.toLowerCase().replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace("-", "").replace("_", ""));
+};
+
+// Convert Currency
+export const convertToCurrency = (num, currency = "EUR", locale = "nl-NL") => {
+  const formatter = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  });
+
+  return formatter.format(num);
 };
---
+++ readme.md
@@ -0,0 +1,145 @@
+## Installation
+
+---
+
+Using npm:
+
+```shell
+$ npm i smn-utils --save
+```
+
+Using yarn:
+
+```shell
+$ yarn add smn-utils
+```
+
+Uses:
+
+```js
+// Time Stamp
+// Exp: 1676689908101
+import { TimeStamp } from "smn-utils";
+TimeStamp();
+
+// TimeStamp with prefix
+// Exp: ABC1676689908101
+import { TimeStamp } from "smn-utils";
+TimeStamp({ prefix: "ABC" });
+
+// TimeStamp with suffix
+// Exp: 1676689908101DEF
+import { TimeStamp } from "smn-utils";
+TimeStamp({ suffix: "DEF" });
+
+// Unique Number
+// Exp: 1676690698596591
+import { Unique } from "smn-utils";
+Unique();
+
+// Unique Number with prefix
+// Exp: ABC1676690698596591
+import { Unique } from "smn-utils";
+Unique({ prefix: "ABC" });
+
+// Unique Number with suffix
+// Exp: 1676690698596591DEF
+import { Unique } from "smn-utils";
+Unique({ suffix: "DEF" });
+
+// Ellipsis with big string
+// Default length value is 30
+// Ellipsis count default value 3
+// Exp: It is a long...
+import { Ellipsis } from "smn-utils";
+Ellipsis("It is a long established fact that a reader will be distracted.", 12, 3);
+
+// Convert second number to readable hour:minute:second
+// Exp: 01:01:30
+import { ConvertHMS } from "smn-utils";
+ConvertHMS(3690);
+
+// Convert second number to readable minute:second
+// Exp: 01:30
+import { ConvertMS } from "smn-utils";
+ConvertMS(90);
+
+// Close current window and show blank screen
+// It only work on browser
+import { WindowClose } from "smn-utils";
+WindowClose();
+// Close current window and redirect to the page
+// It only work on browser
+import { WindowClose } from "smn-utils";
+WindowClose("https://google.com");
+
+// First letter uppercase
+// Exp: Hello world
+import { UCFirst } from "smn-utils";
+UCFirst("hello world");
+
+// Validate email address
+// Exp: true
+import { ValidateEmail } from "smn-utils";
+ValidateEmail("sumonskys@gmail.com");
+
+// wait specific second in async function
+// Wait one second
+import { Sleep } from "smn-utils";
+await Sleep(1);
+
+// Run function multiple time
+// Default run times is 2
+import { RunFunc } from "smn-utils";
+const functionName = () => {
+  console.log("Hello world");
+};
+RunFunc(functionName, 3);
+
+// Run function after specific second
+// Default run times is 2 second
+import { RunDelayFunc } from "smn-utils";
+const functionName2 = () => {
+  console.log("Hello world");
+};
+RunDelayFunc(functionName2, 3);
+
+// Run function and get the run time
+// Exp: functionName: 0.43ms
+import { FuncRunTime } from "smn-utils";
+const functionName3 = () => {
+  console.log("Hello world");
+};
+FuncRunTime(functionName3, "functionName");
+
+// Random number generate with range
+// Default lower value is: 1 and upper value is 100
+// Exp: 47
+import { RandomRange } from "smn-utils";
+RandomRange(1, 100);
+
+// Remove duplicate from array
+// Exp: ["Tom", "Simon", "Sarah"]
+import { RDFA } from "smn-utils";
+RDFA(["Tom", "Simon", "Tom", "Sarah"]);
+
+// String to slug
+// Exp: hello-world
+import { Slugify } from "smn-utils";
+Slugify("Hello World");
+
+// Camel case to snake case
+// Exp: _hello _world
+import { CCTSC } from "smn-utils";
+CCTSC("Hello World");
+
+// Snake case to camel case
+// Exp: Hello World
+import { SCTCC } from "smn-utils";
+SCTCC("_hello _world");
+
+// Convert currency from amount
+// Exp: $101.50
+import { convertToCurrency } from "smn-utils";
+convertToCurrency(101.5, "USD", "us-US");
+```
